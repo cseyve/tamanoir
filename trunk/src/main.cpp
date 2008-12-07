4566bc06ee9d26a423000da29854ce3e957c2b20
cseyve@free.fr <cseyve@free.fr@111133a6-6450-0410-b893-6519ff8e4131>
2008-12-07 19:13:48 +0000

    display fix

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -114,9 +114,8 @@ int main(int argc, char *argv[])
 	
 	
 	QApplication a(argc, argv);
-	//a.setFont(QFont("helvetica", 12));
-  
- 	QTranslator tor( 0 );
+	QTranslator tor( 0 );
+
   	// set the location where your .qm files are in load() below as the last parameter instead of "."
   	// for development, use "/" to use the english original as
   	// .qm files are stored in the base project directory.
