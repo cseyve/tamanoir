9d87e6619aba6eeb675453e8343ac66a9232f300
cseyve <cseyve@free.fr>
2013-12-31 11:40:55 +0000

    Added a script to deploy easily on MacOS X, to get the Mavericks release ready

---
+++ deploy_mac.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+rm -fr Tamanoir.app
+/Developer/Tools/Qt/qmake Tamanoir.pro -r -spec macx-g++ CONFIG+=release
+make clean && make || exit
+/Developer/Tools/Qt/lrelease Tamanoir_French.ts
+SVNVERSION=$(svnversion -n . | sed s#\:#-#g)
+echo "Generating version = $SVNVERSION"
+sed s#VERSION#"$SVNVERSION"#g Info.plist > Tamanoir.app/Contents/Info.plist
+
+echo "Generate .app"
+cp Tamanoir_French.qm ftplogin.cfg Agora.app/Contents/MacOS/
+cp /Developer/Applications/Qt/translations/qt_fr.qm Tamanoir.app/Contents/MacOS/
+cp -r doc Tamanoir.app/Contents/MacOS/
+
+/Developer/Tools/Qt/macdeployqt Tamanoir.app -dmg
+mv Tamanoir.dmg Tamanoir-"$SVNVERSION".dmg
+
+/Developer/Tools/Qt/macdeployqt Tamanoir.app
+zip -r Tamanoir-MacOSX-"$SVNVERSION".zip Tamanoir.app
+echo
+echo "done."
+
+
